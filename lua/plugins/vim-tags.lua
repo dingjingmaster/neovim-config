ac0ee07bcc4bd195ce4e606d6901e198453de665
dingjingmaster <dingjing@live.cn>
2023-09-05 15:23:02 +0800

    添加完整路径

--- lua/plugins/vim-tags.lua
+++ lua/plugins/vim-tags.lua
@@ -5,10 +5,10 @@ return {
         G.cmd([[
             let curDir = expand('%:p:h')
             let name = substitute(curDir, '/', '_', 'g')
-            let g:tagFile = '~/.cache/tags/ctag' . name . '.tags'
-            let g:tags_command = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags  -R -f ~/.cache/tags/ctag' .. name .. '.tags'
-            let g:gtags_command = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags  -R -f ~/.cache/tags/ctag' .. name .. '.tags'
-            autocmd VimEnter * execute 'set tags=' . g:tagFile
+            let tagFile = '~/.cache/tags/ctag' . name . '.tags'
+            let g:tags_command  = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags --tag-relative=never -R -o ~/.cache/tags/ctag' .. name .. '.tags'
+            let g:gtags_command = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags --tag-relative=never -R -o ~/.cache/tags/ctag' .. name .. '.tags'
+            autocmd VimEnter * execute 'set tags=' . tagFile
         ]])
     end
 }
