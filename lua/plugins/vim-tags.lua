e606133db60d09edd3777963887f0ab64115a8e4
dingjingmaster <dingjing@live.cn>
2023-09-05 18:08:18 +0800

    配置自动生成 ctags OK

--- lua/plugins/vim-tags.lua
+++ lua/plugins/vim-tags.lua
@@ -6,9 +6,11 @@ return {
             let curDir = expand('%:p:h')
             let name = substitute(curDir, '/', '_', 'g')
             let tagFile = '~/.cache/tags/ctag' . name . '.tags'
-            let g:tags_command  = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags --tag-relative=never -R -o ~/.cache/tags/ctag' .. name .. '.tags'
-            let g:gtags_command = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+niazS --output-format=e-ctags --tag-relative=never -R -o ~/.cache/tags/ctag' .. name .. '.tags'
+            let g:tags_command = 'ctags --sort=yes --extras=+fFgpqrs --c-kinds=+px --fields=+aCeEfFikKlmnNpPrRsStxzZ --if0=yes --output-format=e-ctags --tag-relative=never -R -o ~/.cache/tags/ctag' . name . '.tags'
+            let g:gtags_command = g:tags_command
+            autocmd VimEnter * execute 'GenerateTags'
             autocmd VimEnter * execute 'set tags=' . tagFile
+            autocmd BufWritePost * call system(g:tags_command)
         ]])
     end
 }
